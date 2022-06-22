3aa5aec08ad4143e9074e47f7d68db030d0e36bc
yukimachino3 <yuki.machino@nutmeglabs.com>
2022-06-22 09:36:32 +0900

    add log

--- src/index.js
+++ src/index.js
@@ -171,7 +171,7 @@ export default class LinkTool {
    * @returns {object} this.nodes.wrapper - render element wrapper
    */
   render() {
-    console.log('test 3');
+    console.log('test 4');
     this.nodes.wrapper = this.make('div', this.CSS.baseClass);
     this.nodes.container = this.make('div', this.CSS.container);
 
@@ -715,9 +715,14 @@ export default class LinkTool {
         li.addEventListener('click', () => {
           this.showModal(media.url);
         });
-        li.appendChild(this.make('img', null, { src: media.url }));
-        this.reviewNodes.itemPicList.appendChild(li);
-        this.pic.push(li);
+
+        const img = this.make('img', null, { src: media.url });
+
+        img.onload = () => {
+          li.appendChild();
+          this.reviewNodes.itemPicList.appendChild(li);
+          this.pic.push(li);
+        };
       });
     }
     if (meta.guest_nickname) {
